470de2579e078c71e9200fe9d3ec9a92166eb892
Adan Trejo <L21550385@chihuahua2.tecnm.mx>
2024-11-19 17:57:41 -0600

    Practica-3

---
+++ Practicas-JS/P3.js
@@ -0,0 +1,20 @@
+// Bienvenida y Solicitar datos al usuario
+let nombre = prompt("¿Cual es tu nombre?");
+alert("Bienvenido,"+ nombre + "!");
+
+let actividad = prompt(nombre + "por favor, ingresa tu actividad favorita:");
+
+//Confirmar si desea guardar la informacion
+let confirm = prompt("¿Desea guardar la  informacion? (Escribe 'si' o 'no')");
+
+// Mostrar mensaje personalizado en una alerta
+alert(`Hola ${nombre}, Tu actividad favorita es ${actividad}`);
+
+// Verificar la respuesta y mostrar el mensaje final correspondiente
+let eliminar = confirm("¿Estas seguro de que tu informacion es correcta?");
+if (eliminar) {
+    alert("Su informacion ha sido guardada correctamente: Nombre: ${nombre}, Actividad Favorita: ${actividad}");
+} else {
+    alert("Su informacion ha sido borrada");
+}
+
