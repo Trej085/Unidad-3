44daf5e234001f6da48d7eaa8e5b9edbc2b234a2
Adan Trejo <L21550385@chihuahua2.tecnm.mx>
2024-11-19 23:05:55 -0600

    Practica-3

--- Practicas-JS/P3.js
+++ Practicas-JS/P3.js
@@ -4,12 +4,15 @@ alert("Bienvenido,"+ nombre + "!");
 
 let actividad = prompt(nombre + "  ingresa tu actividad favorita:");
 
+//
+let respuesta = confirm("¿Estas seguro de que desea guardar la informacion?");
+
 // Verificar la respuesta y mostrar el mensaje final correspondiente
-let confirm = prompt("¿Estas seguro de que tu informacion es correcta?");
-if (confirm) {
+let eliminar = confirm("¿Estas seguro de que tu informacion es correcta?");
+if (eliminar) {
     alert("Su informacion ha sido guardada correctamente:");
     alert(`${nombre}, Tu actividad favorita es ${actividad}`);
-} else {
+} else{
     alert("Su informacion ha sido borrada");
 }
 
