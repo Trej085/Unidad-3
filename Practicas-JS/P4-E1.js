3b2a6e71b7552a66afedecfdb7e83d6c9e5a016b
Adan Trejo <L21550385@chihuahua2.tecnm.mx>
2024-11-25 11:22:42 -0600

    P4-E1

--- Practicas-JS/P4-E1.js
+++ Practicas-JS/P4-E1.js
@@ -1,32 +1,32 @@
 let horario = {
   Materia1: {
     nombre: "Ecuaciones Diferenciales",
-    horario: "Lunes 10:00 - 11:00"
+    hora: "Lunes a Viernes de 10:00 - 11:00"
   },
   Materia2: {
     nombre: "Arquitectura de Computadoras",
-    horario: "Lunes 01:00 - 02:00"
+    hora: "Lunes a Viernes de 01:00 - 02:00"
   },
   Materia3: {
     nombre: "Taller de Base de Datos",
-    horario: "Miércoles 02:00 - 03:00"
+    hora: "Lunes a Jueves de 02:00 - 03:00"
   },
   Materia4: {
     nombre: "Programación Web",
-    horario: "Jueves 05:00 - 06:00"
+    hora: "Lunes a Viernes de 05:00 - 06:00"
   },
   Materia5: {
     nombre: "Lenguajes y Autómatas",
-    horario: "Jueves 06:00 - 07:00"
+    hora: "Lunes a Viernes de 06:00 - 07:00"
   },
 
   // Metodo para imprimir el horario
   imprimirHorario: function () {
-    return this.Materia1.nombre + ": " + this.Materia1.horario + "\n" +
-      this.Materia2.nombre + ": " + this.Materia2.horario + "\n" +
-      this.Materia3.nombre + ": " + this.Materia3.horario + "\n" +
-      this.Materia4.nombre + ": " + this.Materia4.horario + "\n" +
-      this.Materia5.nombre + ": " + this.Materia5.horario;
+    return this.Materia1.nombre + ": " + this.Materia1.hora + "\n" +
+      this.Materia2.nombre + ": " + this.Materia2.hora + "\n" +
+      this.Materia3.nombre + ": " + this.Materia3.hora + "\n" +
+      this.Materia4.nombre + ": " + this.Materia4.hora + "\n" +
+      this.Materia5.nombre + ": " + this.Materia5.hora;
   }
 };
 
