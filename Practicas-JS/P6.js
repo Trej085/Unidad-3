a6f5eaa264c46624dc8fd53bfb6b7152dfe41da8
Adan Trejo <L21550385@chihuahua2.tecnm.mx>
2024-11-27 11:26:18 -0600

    P6

--- Practicas-JS/P6.js
+++ Practicas-JS/P6.js
@@ -1,4 +1,7 @@
 function cambiarDiseño(){
     let titulo = document.getElementById("titulo");
     titulo.innerText = "Manipulacion del DOOM";
+    
+    let contenedor = document.querySelector("#contenedor");
+    contenedor.innerHTML = "<p> Botón al final de la pagina centrado. Cuando se le de click al Boton debera de Cambiar la mayoría de las características.</p>";
 }
