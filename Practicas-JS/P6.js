3f61e7ab6652abead2ff413846cbb7c066d8ea60
Adan Trejo <L21550385@chihuahua2.tecnm.mx>
2024-11-27 23:16:28 -0600

    Practica 6

--- Practicas-JS/P6.js
+++ Practicas-JS/P6.js
@@ -2,6 +2,9 @@ function cambiarDiseño() {
     let titulo = document.getElementById("titulo");
     titulo.innerText = "Manipulacion del DOOM";
 
-    let descripcion = document.getElementById("descripcion");
-    descripcion.innerText = " Botón al final de la pagina centrado. Cuando se le de click al Boton debera de Cambiar la mayoría de las características.";
+    let nombrePractica = document.getElementById("nombrePractica");
+    nombrePractica.innerText = "Practica 6";
+
+    let Descripcion = document.getElementById("Descripcion");
+    Descripcion.innerText = " Botón al final de la pagina centrado. Cuando se le de click al Boton debera de Cambiar la mayoría de las características.";
 }
