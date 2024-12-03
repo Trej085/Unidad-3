e945614091a4ae7abcd910bbeb6c69c4eaefe171
Adan Trejo <L21550385@chihuahua2.tecnm.mx>
2024-12-03 16:31:53 -0600

    Practica 8

--- Practicas-JS/P8.js
+++ Practicas-JS/P8.js
@@ -5,7 +5,7 @@ const suma = (a, b) => a + b;
 const esPar = (num) => num % 2 === 0;
 
  // Función para manejar el botón de suma
- function Suma() {
+ function MostrarSuma() {
     const num1 = parseInt(prompt("Ingrese el primer número:"));
     const num2 = parseInt(prompt("Ingrese el segundo número:"));
     alert(`La suma de ${num1} y ${num2} es: ${suma(num1, num2)}`);
@@ -24,7 +24,7 @@ function verificarPar() {
 }
 
 // Ejercicio 3: Transformar array con .map
-function Mapp() {
+function EjercicioMap() {
     let numeros = [2, 4, 6, 8, 10];
     let cubos = numeros.map(num => num ** 3);
     alert(`Números originales: ${numeros}\nCubo: ${cubos}`);
