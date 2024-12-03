deedbe11ac4e2170e2c643f704bbe91feb9d4fac
Adan Trejo <L21550385@chihuahua2.tecnm.mx>
2024-12-03 16:21:25 -0600

    Practica 8

--- Practicas-JS/P8.js
+++ Practicas-JS/P8.js
@@ -5,7 +5,7 @@ const suma = (a, b) => a + b;
 const esPar = (num) => num % 2 === 0;
 
  // Función para manejar el botón de suma
- function mostrarSuma() {
+ function Suma() {
     const num1 = parseInt(prompt("Ingrese el primer número:"));
     const num2 = parseInt(prompt("Ingrese el segundo número:"));
     alert(`La suma de ${num1} y ${num2} es: ${suma(num1, num2)}`);
